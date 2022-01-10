31e8ea8927b31af419cdec59230ef1e711d769c5
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2022-01-10 04:45:18 -0800

    Peak element - element that is strictly greater than its neighbors

---
+++ CodingQuestions/CodingQuestions/PeakElement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingQuestions
+{
+    internal class PeakElement
+    {
+        public int getPeakElement(int[] array)
+        {
+            int n = array.Length;
+            int l = 0, h = n - 1;
+
+
+            while(l < h)
+            {
+                int mid = (l + h) / 2;
+                if(array[mid] > array[mid+1])
+                {
+                    h = mid;
+                } else
+                {
+                    l = mid+1;
+                }
+            }
+            return array[l];
+        }
+    }
+}
